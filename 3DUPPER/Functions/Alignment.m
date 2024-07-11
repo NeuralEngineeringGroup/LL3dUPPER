48029c481474b15d4c8605f2016f41df0231aa83
AlexDenman <47120300+AlexDenman@users.noreply.github.com>
2024-07-11 08:14:01 -0400

    Minor renaming for consistency with other modified functions

--- 3DUPPER/Functions/Alignment.m
+++ 3DUPPER/Functions/Alignment.m
@@ -1,13 +1,13 @@
-function [RawData]=Alignment(RawData,Ref_Pose)
-[Np, Ndim, Num_samples]=size(RawData);
+function [data]=Alignment(data,Ref_Pose)
+[Np, Ndim, Num_samples]=size(data);
 for n = 1:Num_samples
-    Y_sample = squeeze(RawData(:,:,n));
+    Y_sample = squeeze(data(:,:,n));
     indnum = find(~isnan(Y_sample(:,1)));
     if numel(indnum) > max(0.5*Np, Ndim)
         [~, ~, Trans] = procrustes(Ref_Pose(indnum,:), Y_sample(indnum,:), 'Scaling', false,'Reflection',false);
         Y_sample = Trans.b*Y_sample*Trans.T + repmat(Trans.c(1,:),Np,1);
-        RawData(:,:,n) = Y_sample;
+        data(:,:,n) = Y_sample;
     else
-        RawData(:,:,n) = NaN;
+        data(:,:,n) = NaN;
     end
 end
